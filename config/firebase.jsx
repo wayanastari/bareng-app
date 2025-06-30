a9432064146d82d2ca7ed5197a418cce85ec3dc3
Ni Wayan Astari <144464265+wayanastari@users.noreply.github.com>
2025-06-30 21:16:34 +0800

    Update firebase.jsx

--- config/firebase.jsx
+++ config/firebase.jsx
@@ -6,20 +6,18 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCKvib3POX6k5RGa2ePkBYkwtUWEzgIRXY",
-  authDomain: "bareng-app.firebaseapp.com",
-  databaseURL: "https://bareng-app-default-rtdb.firebaseio.com",
-  projectId: "bareng-app",
-  storageBucket: "bareng-app.firebasestorage.app",
-  messagingSenderId: "495611710585",
-  appId: "1:495611710585:web:57676c5574c7107cc718a8",
-  measurementId: "G-BDKVL79DTN",
+  apiKey: ,
+  authDomain: ,
+  databaseURL: ,
+  projectId: ,
+  storageBucket:,
+  messagingSenderId: ,
+  appId: ,
+  measurementId: ,
 };
 
-// Pastikan tidak ada duplikat inisialisasi
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app); // Gunakan ini di Expo Go
